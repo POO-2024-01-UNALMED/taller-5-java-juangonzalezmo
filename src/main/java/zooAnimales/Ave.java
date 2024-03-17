3f36ea0017b1e168c2e3fe2f7592d4f1d2eb31bb
EsteArgen <101402836+EsteArgen@users.noreply.github.com>
2024-03-16 23:10:17 -0500

    Update Ave.java

--- src/main/java/zooAnimales/Ave.java
+++ src/main/java/zooAnimales/Ave.java
@@ -1,24 +1,40 @@
 package zooAnimales;
 import java.util.ArrayList;
-import gestion.Zona;
 
 public class Ave extends Animal{
-	
-	private static ArrayList<Ave> listado = new ArrayList<>();
+	private static ArrayList<Ave> listado=new ArrayList<>();
 	public static int halcones;
 	public static int aguilas;
 	private String colorPlumas;
 	
-	public Ave(String nombre, int edad, String habitat, String genero, String colorPlumas) {
-		super( nombre, edad, habitat, genero);
-		this.colorPlumas = colorPlumas;
-		listado.add(this);
+	public Ave() {
+		this(null,0,null,null,null);
 	}
-
-	public Ave(){listado.add(this);}
 	
-	public static int getNumber() {
-		return listado.size();
+	public Ave(String nombre, int edad, String habitat, String genero, String colorPlumas) {
+		super(nombre,edad,habitat,genero);
+		this.colorPlumas=colorPlumas;
+		Ave.listado.add(this);
+	}
+	
+	
+	public static void setListado(ArrayList<Ave> listado) {
+		Ave.listado=listado;
+	}
+	public static ArrayList<Ave> getListado() {
+		return listado;
+	}
+	
+	public void setColorPlumas(String colorPlumas) {
+		this.colorPlumas=colorPlumas;
+	}
+	public String getColorPlumas() {
+		return colorPlumas;
+	}
+	
+	public int cantidadAves() {
+		return Ave.listado.size();
+		
 	}
 	
 	public String movimiento() {
@@ -26,36 +42,15 @@ public class Ave extends Animal{
 	}
 	
 	public static Ave crearHalcon(String nombre, int edad, String genero) {
+		Ave Ave=new Ave(nombre,edad, "montanas", genero, "cafe glorioso");
 		halcones++;
-		return new Ave(nombre,edad,"montanas",genero,"cafe glorioso");
+		return Ave;
 	}
 	
 	public static Ave crearAguila(String nombre, int edad, String genero) {
+		Ave Ave=new Ave(nombre,edad, "montanas", genero, "blanco y amarillo");
 		aguilas++;
-		return new Ave(nombre,edad,"montanas",genero,"blanco y amarillo");
-	}
-	
-	public int cantidadAves() {
-		return listado.size();
+		return Ave;
 	}
 
-	public static ArrayList<Ave> getListado() {
-		return listado;
-	}
-
-	public String getColorPlumas() {
-		return colorPlumas;
-	}
-
-	public static void setListado(ArrayList<Ave> listado) {
-		Ave.listado = listado;
-	}
-
-	public void setColorPlumas(String colorPlumas) {
-		this.colorPlumas = colorPlumas;
-	}
-	
-	
-	
-	
 }
