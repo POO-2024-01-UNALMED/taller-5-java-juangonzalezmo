4dd1717557ffab74957675cbd4c671093cd69013
EsteArgen <101402836+EsteArgen@users.noreply.github.com>
2024-03-16 23:09:43 -0500

    Update Animal.java

--- src/main/java/zooAnimales/Animal.java
+++ src/main/java/zooAnimales/Animal.java
@@ -1,101 +1,96 @@
 package zooAnimales;
-import gestion.*;
+
+import gestion.Zona;
 
 public class Animal {
-	
 	private static int totalAnimales;
 	private String nombre;
 	private int edad;
 	private String habitat;
 	private String genero;
-	private Zona zona; 
+	private Zona zona;
 	
-	public Animal(String nombre, int edad, String habitat, String genero) {
-		
-		this.nombre = nombre;
-		this.edad = edad;
-		this.habitat = habitat;
-		this.genero = genero;
+	public Animal() {
+		this(null,0,null,null);
+	}
+	
+	public Animal( String nombre, int edad, String habitat, String genero) {
+		this.nombre=nombre;
+		this.edad=edad;
+		this.habitat=habitat;
+		this.genero=genero;
+		this.zona=null;
 		totalAnimales++;
+	
 	}
 	
-	public Animal(){totalAnimales++;}
 	
-	public static String totalPorTipo() {
-		return "Mamiferos: " + (Mamifero.getNumber()+1)+
-				"\nAves: " + (Ave.getNumber()+2)+
-				"\nReptiles: " + (Reptil.getNumber()+1)+
-				"\nPeces: " + (Pez.getNumber()+1)+
-				"\nAnfibios: " + (Anfibio.getNumber()+1);
-				
+	public static void setTotalAnimales(int totalAnimales) {
+		Animal.totalAnimales=totalAnimales;
 	}
-	public String toString() {
-		String statement=null;
-		if(zona==null) {
-			statement= "Mi nombre es "+ nombre+", tengo una edad de "+ edad+", habito en "+ habitat+ " y mi genero es "+genero;
-		}
-		else {
-			statement= "Mi nombre es "+ nombre+", tengo una edad de "+ edad+", habito en "+ habitat+ " y mi genero es "+genero+", la zona en la que me ubico es "+zona.getNombre()+" en el "+zona.getZoo().getNombre();
-		}
-		
-		return statement;
+	public static int getTotalAnimales() {
+		return totalAnimales;
 	}
+	
+	public void setNombre(String nombre) {
+		this.nombre=nombre;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setEdad(int edad) {
+		this.edad=edad;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	
+	public void setHabitat(String habitat) {
+		this.habitat=habitat;
+	}
+	public String getHabitat() {
+		return habitat;
+	}
+	
+	public void setGenero(String genero) {
+		this.genero=genero;
+	}
+	public String getGenero() {
+		return genero;
+	}
+	
+	public void setZona(Zona zona) {
+		this.zona=zona;
+	}
+	public Zona getZona() {
+		return zona;
+	}
+	
 	
 	public String movimiento() {
 		return "desplazarse";
 	}
-
-	public static int getTotalAnimales() {
-		return totalAnimales;
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public int getEdad() {
-		return edad;
-	}
-
-	public String getHabitat() {
-		return habitat;
-	}
-
-	public String getGenero() {
-		return genero;
-	}
-
-	public Zona getZona() {
-		return zona;
-	}
-
-	public static void setTotalAnimales(int totalAnimales) {
-		Animal.totalAnimales = totalAnimales;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public void setEdad(int edad) {
-		this.edad = edad;
-	}
-
-	public void setHabitat(String habitat) {
-		this.habitat = habitat;
-	}
-
-	public void setGenero(String genero) {
-		this.genero = genero;
-	}
-
-	public void setZona(Zona zona) {
-		this.zona = zona;
-	}
-
 	
+	public static String totalPorTipo() {
+		
+		return "Mamiferos: "+Mamifero.getListado().size()+"\n"+ 
+		"Aves: "+Ave.getListado().size()+"\n"+ 
+		"Reptiles: "+Reptil.getListado().size()+"\n" + 
+		"Peces: " +Pez.getListado().size()+"\n"+ 
+		"Anfibios: "+Anfibio.getListado().size();
+		
+		
+	}
+	
+	public String toString() {
+		if (getZona()!=null) {
+			return "Mi nombre es "+ getNombre()+", tengo una edad de " +getEdad()+ ", habito en "+getHabitat()+" y mi genero es "+getGenero()+", la zona en la que me ubico es "+getZona().getNombre()+", en el "+getZona().getZoo().getNombre();
+		}
+		else {
+		    return "Mi nombre es "+ getNombre()+", tengo una edad de " +getEdad()+ ", habito en "+getHabitat()+" y mi genero es "+getGenero();
+		}
+	}
 
-	
-	
-	
+
 }
